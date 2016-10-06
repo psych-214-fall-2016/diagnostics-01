c12483dd53ceff0e6d1366a1fac36fd1fdbd6cb5
Joe Winer <jwiner@berkeley.edu>
2016-10-06 14:44:29 -0700

    Updating pca outliers code

--- scripts/pca_outliers.py
+++ scripts/pca_outliers.py
@@ -13,17 +13,6 @@ img = nibabel.load('data/group01_sub01_run1.nii')
 #- Get the data array from the image
 data = img.get_data()
 
-"""
-The first part of the code determines which voxels are inside the brain
-and which are outside the brain and creates a mask (boolean matrix)
-"""
-
-
-"""
-The second part of the code will use PCA to get component matrix U
-and scalar projections matrix C
-"""
-
 #- Make variables:
 #- 'vol_shape' for shape of volumes
 vol_shape = data.shape[:-1]
@@ -32,11 +21,16 @@ n_vols = data.shape[-1]
 #- N is the number of voxels in a volume
 N = np.prod(vol_shape)
 
+"""
+The first part of the code will use PCA to get component matrix U
+and scalar projections matrix C
+"""
+
 #- Reshape to 2D array that is voxels by volumes (N x n_vols)
 X = data.reshape((N, n_vols))
 
 #- Calculate unscaled covariance matrix for X
-unscaled_covariance = X.dot(X.T)
+unscaled_covariance = X.dot(X.T) """why does this break python?"""
 
 #- Use SVD to return U, S, VT matrices from unscaled covariance
 U, S, VT = npl.svd(unscaled_covariance)
@@ -47,15 +41,31 @@ C = U.T.dot(X)
 
 #- Transpose C
 #- Reshape the first dimension of C to have the 3D shape of the original data volumes.
-C_vols = C.T.reshape(vol_shape + n_vols) """check this - not sure this works"""
+C_vols = C.T.reshape(vol_shape, n_vols) """check this - not sure this works"""
 
 """
-The third part of code finds the root mean square of U, then uses the
-mask from step 1 to determine which components explain data outside
-the brain
+The second part of the code determines which voxels are inside the brain
+and which are outside the brain and creates a mask (boolean matrix)
 """
 
+#boolean mask set to all voxels above .2 in the first volume
+volume_one = data[..., 0]
+vol_mean = np.mean(volume_one)
+mask = volume_one > (.2*vol_mean) """what is the best threshold?"""
+outside_brain = data[~mask] """put this here or wait for C matrix?"""
 
+"""
+The third part of code finds the root mean square of U from step 1, then uses the
+mask from step 2 to determine which components explain data outside the brain
+Selects these "bad components" with high "outsideness"
+"""
+
+#Take first volume from 4D C matrix
+C_vol0 = C[..., 0]
+#Get all voxels that are outside of brain
+voxels_outside = c_vol0[~mask]
+#Get RMS of the C matrix , reflecting intensity of the component outside the brain
+RMS = np.sqrt(np.sum(voxels_outside ** 2))
 
 """
 The fourth part of the code uses the "bad components" to generate a new
